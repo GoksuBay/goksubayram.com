be66926a7f66671738ede25d05058dc878342de4
goksubay <goksu1.9.9.8@gmail.com>
2020-05-24 01:06:04 +0300

    first test

--- my-app/src/components/footer.js
+++ my-app/src/components/footer.js
@@ -6,13 +6,13 @@ class Footer extends Component {
             <div>
               <footer className='social-links'>
               <a href='http://linkedin.com/in/goksubay' rel='noopener noreferrer' target='_blank'>
-                <i className='fab fa-linkedin-in'  aria-hidden='true' />
+                <i className='fab fa-linkedin-in' id='linkedin'  aria-hidden='true' />
               </a>
               <a href='https://github.com/GoksuBay' rel='noopener noreferrer' target='_blank'>
-                <i className='fab fa-github'  aria-hidden='true' />
+                <i className='fab fa-github' id='github' aria-hidden='true' />
               </a>
               <a href='https://twitter.com/Goksubayy' rel='noopener noreferrer' target='_blank'>
-                <i className='fab fa-twitter'  aria-hidden='true' />
+                <i className='fab fa-twitter' id='twitter'  aria-hidden='true' />
               </a>
               </footer>
             </div>
