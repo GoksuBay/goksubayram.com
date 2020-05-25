145fd51f65d3b3713805d99494ecf42aafc0bc8a
goksubay <goksu1.9.9.8@gmail.com>
2020-05-26 02:32:17 +0300

    Main Page completed

--- my-app/src/components/landing.js
+++ my-app/src/components/landing.js
@@ -1,10 +1,21 @@
 import React, {Component} from 'react';
+import {Grid, Cell} from 'react-mdl';
 import '../css/landing.css';
 
 class Landing extends Component {
   render() {
     return(
-      <div className="hcolor"><h1>Welcome Page</h1></div>
+      <div style={{width: '100%', margin: 'auto'}}>
+        <Grid className="grid">
+          <Cell col={12}>
+          <div className="text">
+            <h1>Göksu Bayram</h1>
+            <h2>Computer Engineering Student</h2>
+            <h2>At Akdeniz University</h2>
+            </div>
+           </Cell>
+           </Grid>
+      </div>
     );
   }
 }
